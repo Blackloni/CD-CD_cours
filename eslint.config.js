1aadc5ab0e57cc0e6f2d7eb9d10eafef40461c95
MavrodisMichael <m.mavrodis@gmail.com>
2026-04-20 21:28:50 +0200

    ci: migrate to eslint v9 flat config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+      }
+    }
+  }
+];
